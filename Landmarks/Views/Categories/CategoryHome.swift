8defcf67b21d59651fc8beea9c85f925944c4427
BookShelf <seunghyeoks@naver.com>
2025-01-03 14:56:06 +0900

    practice Chapter 3-1

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,38 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by 김승혁 on 1/3/25.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
