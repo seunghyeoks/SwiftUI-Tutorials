027cce01416c8c7cffcbb31b16ec5d17dbd5392d
BookShelf <seunghyeoks@naver.com>
2025-01-03 15:33:11 +0900

    practice Chapter 3-2

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,29 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by 김승혁 on 1/3/25.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "g_kumar")
+
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
